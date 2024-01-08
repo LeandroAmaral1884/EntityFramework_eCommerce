147321d9fa43b35462cd5967997fdefc4921b3d3
leandro.amaral <amaral4957@gmail.com>
2024-01-08 12:03:58 -0300

    Sql Migration

--- eCommerceApi/Migrations/20240108144925_Mudanca02_Usuario_AddColNomePai.Designer.cs
+++ eCommerceApi/Migrations/20240108144925_Mudanca02_Usuario_AddColNomePai.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using eCommerceApi.Database;
 
@@ -11,9 +12,11 @@ using eCommerceApi.Database;
 namespace eCommerceApi.Migrations
 {
     [DbContext(typeof(eCommerceContext))]
-    partial class eCommerceContextModelSnapshot : ModelSnapshot
+    [Migration("20240108144925_Mudanca02_Usuario_AddColNomePai")]
+    partial class Mudanca02_Usuario_AddColNomePai
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
